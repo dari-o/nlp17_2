9af86783a25c9540a2a0bb16d4e200064d6866c2
dari-o <bozhidara.ognyanova@gmail.com>
2017-06-09 04:40:55 +0100

    embedding average metric added

--- embedding_metrics.py
+++ embedding_metrics.py
@@ -13,7 +13,8 @@ ground_truth_path = 'validation_ground_truth.txt'
 word2int_path = "works" + sep +"netflix" +sep+ "data"+ sep+"word2int_dictionary.npy"
 word2int = np.load(pathToWord2Int).item()
 model_output = open(model_output_path).readlines()
-ground_truth = len(model_output)
+ground_truth = open(ground_truth_path).readlines()
+num_responses = 1000 #len(model_output)
 vocabSize = 20000
 
 embeddingSize=512
@@ -68,6 +69,12 @@ def average_greedy(model_output_emb, ground_truth_emb):
 		sum += GM(model_output_emb[i], ground_truth_emb[i])
 	return sum/num_responses
 
+def emb_avg(sent_emb):
+	sum = np.zeros(embeddingSize)
+	for w in sent_emb:
+		sum = sum+w
+	return sum/len(sent_emb)
+
 session = tf.Session()
 model_output_emb = []
 ground_truth_emb = []
@@ -76,20 +83,32 @@ weightsEmbedding    = tf.Variable(tf.random_uniform([vocabSize, embeddingSize], 
 load_embedding(session, weightsEmbedding)
 
 initialization = tf.global_variables_initializer()
+EA = 0
 session.run(initialization)
 with session.as_default():
 	for i in range(num_responses):
 		#print(i)
+		
 		out_sent_int = sent2ints(model_output[i].split())
 		truth_sent_int = sent2ints(ground_truth[i].split())
 		#pdb.set_trace()
 		x  = tf.nn.embedding_lookup( weightsEmbedding, tf.to_int32(out_sent_int))
+		x = x.eval()
 		x2 = tf.nn.embedding_lookup( weightsEmbedding, tf.to_int32(out_sent_int))
-		model_output_emb.append(x.eval())
-		ground_truth_emb.append(x2.eval())
+		x2 = x2.eval()
+
+		out_emb_avg = emb_avg(x)
+		true_emb_avg = emb_avg(x2)
+		EA += cosine_similarity(true_emb_avg, out_emb_avg)
+		model_output_emb.append(x)
+		ground_truth_emb.append(x2)
 	print("done")
 
 eval = average_greedy(model_output_emb, ground_truth_emb)
+EA = EA/num_responses
 print("CHECK PATHS AND NUM_RESPONSES FOR THE NUMBER OF SENTANCES WE WANT TO EVALUATE")
+print("Greedy Mathcing Score")
 print(eval)
+print("Embedding Average Score")
+print(EA)
 session.close()
